35fb9d2c8bd96e777e77f0a98b00740afc9a5489
jennahopeclem <jennahopeclem@gmail.com>
2023-01-17 12:02:55 -0500

    added to entries handlebar; created html and css for entries page

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
-const homeRoutes = require("./Home-routes");
+const homeRoutes = require("./homeRoutes");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
-=======
-const router = require('express').Router();
-
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
->>>>>>> 4d6c40984a7d9b85e74a4e1956a35389f21e8f00
