33ad120926ddd7d72f67386cc587a44424a904fa
val.gee <vguevara0806@gmail.com>
2023-01-17 09:48:25 -0500

    connected comment seed to database successfully

--- models/index.js
+++ models/index.js
@@ -1,36 +1,35 @@
 const Entry = require('./Entry');
-const Goals = require('./Goals');
+const Goal = require('./Goal');
 const User = require('./User');
-const Comments = require('./Comments');
+const Comment = require('./Comment');
 
 User.hasMany(Entry, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
-User.hasMany(Goals, {
+User.hasMany(Goal, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
-
-User.hasMany(Comments, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-})
 
 Entry.belongsTo(User, {
     foreignKey: 'user_id'
 }
 );
 
-Entry.hasMany(Comments, {
+Entry.hasMany(Comment, {
     foreignKey: 'entry_id',
     onDelete: 'CASCADE'
 }
 );
 
-Goals.belongsTo(User, {
+Comment.belongsTo(Entry, {
+    foreignKey: 'entry_id'
+});
+
+Goal.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { Entry, Goals, User };
+module.exports = { Entry, Goal, User, Comment };
