5f29e789728c60ce38a463921a14a5103e7cfbd0
michelle <113802263+michelletrn@users.noreply.github.com>
2023-01-17 22:29:21 -0500

    fixed routing typos

--- controllers/api/User-routes.js
+++ controllers/api/User-routes.js
@@ -19,7 +19,7 @@ router.post("/", async (req, res) => { //works
 
 router.post("/login", async (req, res) => {
   try {
-    const userData = await User.findone({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
