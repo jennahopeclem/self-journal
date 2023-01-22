c59706f7e01c35f70868cc4c9a2922656fdae2c0
val.gee <vguevara0806@gmail.com>
2023-01-22 18:12:04 -0500

    added styling to cards

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,11 +6,5 @@ module.exports = {
   format_time: (date) => {
     // We use the 'toLocaleTimeString()' method to format the time as H:MM:SS AM/PM
     return date.toLocaleTimeString();
-  },
-
-  todays_date: () => {
-    let date = new Date().toDateString();
-
-    return date;
-  },
+  }
 };
